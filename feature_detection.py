41fa35af2e2ce69b09b9814198194f3e4821b528
tejismanp <tejismanp001@gmail.com>
2017-03-18 15:57:00 +0530

    feature detection using SIFT

---
+++ feature_detection.py
@@ -0,0 +1,54 @@
+import numpy as np
+import cv2
+
+
+img1 = cv2.imread('2.jpg', 0)
+img2 = cv2.imread('4.jpg', 0)
+
+# Detect the SIFT key points and compute the descriptors for the two images
+sift = cv2.xfeatures2d.SIFT_create()
+keyPoints1, descriptors1 = sift.detectAndCompute(img1, None)
+keyPoints2, descriptors2 = sift.detectAndCompute(img2, None)
+
+# Create brute-force matcher object
+bf = cv2.BFMatcher()
+
+# Match the descriptors
+matches = bf.knnMatch(descriptors1, descriptors2, k=2)
+
+
+goodMatches = []
+
+for m, n in matches:
+    if m.distance < 0.7 * n.distance:
+        goodMatches.append(m)
+ 
+MIN_MATCH_COUNT = 10
+
+if len(goodMatches) > MIN_MATCH_COUNT:
+    
+    sourcePoints = np.float32([ keyPoints1[m.queryIdx].pt for m in goodMatches ]).reshape(-1, 1, 2)
+    destinationPoints = np.float32([ keyPoints2[m.trainIdx].pt for m in goodMatches ]).reshape(-1, 1, 2)
+    
+    
+    M, mask = cv2.findHomography(sourcePoints, destinationPoints, method=cv2.RANSAC, ransacReprojThreshold=5.0)
+    matchesMask = mask.ravel().tolist()
+    
+        h, w = img1.shape
+    corners = np.float32([ [0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0] ]).reshape(-1, 1, 2)
+    transformedCorners = cv2.perspectiveTransform(corners, M)
+    
+    
+    img2 = cv2.polylines(img2, [np.int32(transformedCorners)], True, (255, 255, 255), 2, cv2.LINE_AA)
+else:
+    print("Not enough matches are found - %d/%d" % (len(goodMatches), MIN_MATCH_COUNT))
+    matchesMask = None
+
+
+drawParameters = dict(matchColor=(0, 255, 0), singlePointColor=None, matchesMask=matchesMask, flags=2)
+result = cv2.drawMatches(img1, keyPoints1, img2, keyPoints2, goodMatches, None, **drawParameters)
+
+
+cv2.imshow('Homography', result)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
